b001f7712d5964493c830b0a6b47f5fa45053277
Shiva Prasad Adirala <shivaprasad.adirala@CaprusIT.local>
2020-04-08 12:23:59 +0530

    Version upgrade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
       name='robotframework-historic-parser',
-      version="0.1.4",
+      version="0.1.7",
       description='Parser to push robotframework execution results to MySQL',
       classifiers=[
           'Framework :: Robot Framework',
